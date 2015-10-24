11b2ec3d131139e23b62ccf2ad02ed8aaecd13c6
unknown <hydra0@bigmir.net>
2015-10-25 01:19:04 +0300

    initial commit

--- package.js
+++ package.js
@@ -13,7 +13,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.0.2');
   api.use('jquery');
-  api.use('percolate:velocityjs');
+  api.use('percolate:velocityjs@1.2.1_1');
   api.addFiles('lib/jpreloader.js', 'client');
   api.addFiles('lib/jpreloader.css', 'client');
 });
