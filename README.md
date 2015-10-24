0f7f1b9afda5f83fb63e7d0827fe2a904526c2eb
unknown <hydra0@bigmir.net>
2015-10-25 01:10:31 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# meteor-jpreloader
+# meteor-translit
+Preloading screen (aka. splash screen) for your website with velocty.js animations. Thanks to https://github.com/kennyooi/jpreloader and https://atmospherejs.com/percolate/velocityjs
+
+## Usage
+
+```
+Template.layout.onRendered ->
+  $('body').jpreLoader()
+```
---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  name: 'hydraorc:jpreloader',
+  version: '1.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Preloading screen (aka. splash screen) for your website. Thanks to https://github.com/kennyooi/jpreloader and https://atmospherejs.com/percolate/velocityjs',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/hydraorc/meteor-jpreloader.git',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.0.2');
+  api.use('jquery');
+  api.use('percolate:velocityjs');
+  api.addFiles('lib/jpreloader.js', 'client');
+  api.addFiles('lib/jpreloader.css', 'client');
+});
